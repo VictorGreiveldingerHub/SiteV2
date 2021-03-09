76dcc3c3d7a959e17446cce8b04e7bcaa8466ed4
Greiveldinger Victor <victorgreiveldinger@gmail.com>
2021-03-09 13:10:52 +0100

    Mise en place formulaire avec parallax essaye de mise en place de nodemailer avec express et node

---
+++ api/merci_template.js
@@ -0,0 +1,22 @@
+const Merci = data => {
+
+    return `
+        <!DOCTYPE html>
+        <html style="margin: 0; padding: 0;">
+
+        <head>
+            <title>Merci</title>
+        </head>
+
+            <body style="margin: 0; padding: 0;">
+                <br />
+                <br />
+                <div>Merci</div>
+                <br />
+                <br />
+            </body>
+        </html>
+    `;
+};
+
+module.exports = {Merci};
