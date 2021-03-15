18a1e4c41d0a85231247fa803141ac04f6bd84d1
Greiveldinger Victor <victorgreiveldinger@gmail.com>
2021-03-15 04:18:23 +0100

    Ajout favicon

--- web/src/components/Skills/index.js
+++ web/src/components/Skills/index.js
@@ -17,24 +17,30 @@ const Skills = () => {
         <div className="skills">
             <section className="global-section">
                 <h3 className="skills-h3">S<span className="h3-span">oft</span></h3>
-                <motion.div
-                    className="skills-div"
-                   
-                >
+                <motion.div className="skills-div">
                     <div>
-                        Pendant ma formation, j'ai pu m'entrainer à utiliser Linux, la ligne de commande & Git pour faciliter le travail en groupe
+                        Linux, les lignes de commande & Git.
                     </div>
                     <div>
-                        avec l'utilisation de Github pour la gestion des hébergements et la gestion de développement logiciel.
+                        Github pour la gestion des hébergements et la gestion de développement logiciel.
                     </div>
                     <div>
                         Grâce aux nombreux ateliers à chaque fin de cours, nous étions libre de travailler en équipe, avec Discord et LiveShare de VsCode.
                     </div>
                     <div>
-                        Le tout pour nous imprégner le plus possible avec la notion de méthode Agile SCRUM, que nous aurons tous exploiter lors de notre projet de fin d'étude dont voici la présentation
-                        <motion.button>
-                            Hold
-                        </motion.button>
+                        Le tout pour nous imprégner le plus possible avec la notion de méthode Agile SCRUM, que nous aurons tous exploiter lors de notre projet de fin d'étude (en groupe de 4).
+                    </div>
+                    <div>
+                        Gestion de projet avec conception d'un cahier des charges, de base de donnée, user stories, wireframes, MVC (Model-View-Controller) sont des notions familiaire
+                    </div>
+                    <div>
+                        <h3>Suite à la formation</h3>
+                        <div>
+                            Opquast
+                        </div>
+                        <div>
+                            ElAO
+                        </div>
                     </div>
                 </motion.div>
             </section>
@@ -42,25 +48,41 @@ const Skills = () => {
                 <h3 className="skills-h3">F<span className="h3-span">ront-end</span></h3>
                 <div className="skills-div">
                     <div>
-                        Cette formation a commencé avec le HTML et le CSS, langages de création d'une page web ...
+                        HTML et CSS ...
                     </div>
                     <div>
-                        puis est venu le tour de Javascript (avec la création des premiers algorithmes), orienté côté client, et en complément du HTML/CSS, pour l'intéractivité dans nos pages web & web mobile. 
+                        Javascript, API, AJAX, POO ...
                     </div>
                     <div>
-                        Grâce aux nombreux ateliers à chaque fin de cours, nous étions libre de travailler en équipe, avec Discord et LiveShare de VsCode.
+                        React, React-Router, Redux, store, gestion de state, Hooks ...
                     </div>
                     <div>
-                        Le tout pour nous imprégner le plus possible avec la notion de méthode Agile SCRUM, que nous aurons tous exploiter lors de notre projet de fin d'étude dont voici la présentation
+                        <h4>Récemment</h4>
+                        <div>
+                            Apprentissage de ThreeJs, React Three Fiber, WebGl, framer-motion, Locomotive-Scroll ... 
+                        </div>
                     </div>
-                    Utilisation du HTML5, CSS3, Javascript, React
                 </div>
             </section>
             <section className="backend-section">
                 <h3 className="skills-h3">B<span className="h3-span">ack-end</span></h3>
                 <div className="skills-div">
-                    Utilisation de NodeJs, Express
+                    <div>
+                        NodeJs & Express
+                    </div>
+                    <div>
+                        SQL ...
+                    </div>
+                    <div>
+                        <h4>A venir</h4>
+                        <div>
+                            Apprentissage de PHP
+                        </div>
+                    </div>
                 </div>
+            </section>
+            <section>
+                N'hésitez pas à regarder plus en détail ma formation BOUTON 
             </section>
         </div>
         </motion.div>
